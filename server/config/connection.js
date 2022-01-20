136a153cb7a7644dc68f270c63c558a275e21597
Aisha Osman <aisha.osman09@gmail.com>
2022-01-20 10:31:24 -0600

    deploy

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,11 +1,8 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/deep-thoughts',
-  {
-    useNewUrlParser: true, 
-    useUnifiedTopology: true,
-  }
-);
-
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/deep-thoughts', 
+{ useNewUrlParser: true,
+  useUnifiedTopology: true, })
+        .then(connect => console.log('connected to mongodb..'))
+        .catch(e => console.log('could not connect to mongodb', e))
 module.exports = mongoose.connection;
