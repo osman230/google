4ca6025b976746ed9d6fa3a1adecdc5f44fcb532
Aisha Osman <aisha.osman09@gmail.com>
2022-01-20 20:12:20 -0600

    query

---
+++ client/src/utils/queries.js
@@ -0,0 +1,24 @@
+import { gql } from '@apollo/client';
+
+
+export const QUERY_ME = gql`
+  {
+    me {
+        _id
+        username
+        email
+        bookCount
+        savedBooks {
+            bookId
+            title
+            description
+            authors
+            link
+            image
+        }
+      }
+    }
+  }
+`;
+
+
